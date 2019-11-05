e1f6751a77d0a7e9ebdc1df126ebbd63462d8e24
Andrei <andrei@laye.red>
2019-11-05 17:38:48 +0100

    Added - options page & icon

---
+++ src/options/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>No options yet..</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
