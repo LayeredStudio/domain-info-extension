988da8b29711de5d4edeb849e664b4be8c2540f2
Andrei <andrei@laye.red>
2025-10-05 19:14:39 +0200

    updated: dev deps

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' assert { type: 'json' }
+import manifest from './manifest.json' with { type: 'json' }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    crx({ manifest }),
-  ],
+	plugins: [vue(), crx({ manifest })],
+	server: {
+		cors: {
+			origin: [/chrome-extension:\/\//],
+		},
+	},
 })
